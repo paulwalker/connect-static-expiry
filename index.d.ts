72111a6695a817e456215b20386613a9ee8a93ef
Ethan Resnick <ethan.resnick@gmail.com>
2016-11-26 00:07:53 -0800

    Add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="connect" />
+/// <reference types="node" />
+import { HandleFunction, NextHandleFunction } from "connect";
+import { Stats } from "fs";
+
+declare function staticExpiry(app: staticExpiry.app, config: staticExpiry.config): NextHandleFunction;
+
+declare namespace staticExpiry {
+  type fileFilterCb = (fileName: string, stat: Stats) => boolean;
+
+  // Static expiry works with express and connect, which return app objects
+  // of different shapes. So app.use() is the common interface relied on.
+  export type app = {
+    use(route: string, fn: HandleFunction): app;
+  };
+
+  export type config = {
+    duration?: number;
+    conditional?: "none" | "etag" | "last-modified" | "both";
+    unconditional?: "none" | "max-age" | "expires" | "both";
+    cacheControl?: string | false | null;
+    dir?: string;
+    fingerprint?: (filePath: string) => string;
+    location?: 'prefile' | 'postfile' | 'query' | 'path';
+    host?: null | string | string[];
+    loadCache?: 'startup' | 'furl' | { at: 'startup', callback: fileFilterCb };
+    debug?: boolean;
+  };
+
+  export function clearCache(): void;
+}
+
+export= staticExpiry;
