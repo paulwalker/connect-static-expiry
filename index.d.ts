87354eb859b503364d8d206130b71b258b3a2e80
Ethan <ethan.resnick@gmail.com>
2017-04-04 17:26:35 -0400

    Update typescript typings to account for exposed furl + possibly missing app
    Should have been part of #17

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,15 @@
 import { HandleFunction, NextHandleFunction } from "connect";
 import { Stats } from "fs";
 
-declare function staticExpiry(app: staticExpiry.app, config: staticExpiry.config): NextHandleFunction;
+declare function staticExpiry(app: staticExpiry.app | null | undefined, config: staticExpiry.config): staticExpiry.Middleware;
 
 declare namespace staticExpiry {
   type fileFilterCb = (fileName: string, stat: Stats) => boolean;
+  type furl = (filePath: string) => string;
+
+  interface Middleware extends NextHandleFunction {
+    furl: furl
+  }
 
   // Static expiry works with express and connect, which return app objects
   // of different shapes. So app.use() is the common interface relied on.
@@ -20,7 +25,7 @@ declare namespace staticExpiry {
     unconditional?: "none" | "max-age" | "expires" | "both";
     cacheControl?: string | false | null;
     dir?: string;
-    fingerprint?: (filePath: string) => string;
+    fingerprint?: furl;
     location?: 'prefile' | 'postfile' | 'query' | 'path';
     host?: null | string | string[];
     loadCache?: 'startup' | 'furl' | { at: 'startup', callback: fileFilterCb };
